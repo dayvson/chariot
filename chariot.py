f9512c6aa03dd7c71a60c4fe5a02c1c47a46cd77
Maxwell Dayvson <dayvson@gmail.com>
2011-11-21 04:05:35 -0200

    Starting flask application

---
+++ chariot.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def direction():
+    return "Hello world!"
+if __name__ == '__main__':
+    app.run()
