21406733e7d32956cfa2a9ce4aacbf2c285d89a1
Maxwell Dayvson <dayvson@gmail.com>
2011-11-22 17:32:46 -0200

    Adding new schema data to Cassandra

--- data/schema.py
+++ data/schema.py
@@ -26,11 +26,13 @@ class Schema(object):
         
     def createSchema(self):
         self.sys.create_keyspace('Chariot', SIMPLE_STRATEGY, {'replication_factor': '1'})
-        self.sys.create_column_family('Chariot', 'MMedia', comparator_type=UTF8_TYPE)
-        self.sys.create_column_family('Chariot', 'Sources', comparator_type=BYTES_TYPE)
-        self.sys.create_column_family('Chariot', 'Mirrors', comparator_type=BYTES_TYPE)
-        self.sys.create_column_family('Chariot', 'GeoCountry', comparator_type=UTF8_TYPE)
-        self.sys.create_column_family('Chariot', 'GeoCountryMirrors', comparator_type=UTF8_TYPE)
+        self.sys.create_column_family('Chariot', 'VOD', comparator_type=UTF8_TYPE)
+        self.sys.create_column_family('Chariot', 'LIVE', comparator_type=UTF8_TYPE) #TIME_UUID_TYPE)
+        self.sys.create_column_family('Chariot', 'MULTIMEDIA', comparator_type=UTF8_TYPE)
+        self.sys.create_column_family('Chariot', 'EDGENODE', comparator_type=UTF8_TYPE)
+        self.sys.create_column_family('Chariot', 'CDNCOUNTRY', super=True, comparator_type=UTF8_TYPE)
+        self.sys.create_column_family('Chariot', 'BGP', comparator_type=UTF8_TYPE)
+        self.sys.create_column_family('Chariot', 'FARM', comparator_type=UTF8_TYPE)
              
     def sync(self):
         if not self.hasSchema():
